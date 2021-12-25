981cb4f503af0b594750bd2f4ccfd93ccbb9f78e
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-25 22:02:59 +0330

    init

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+
+const API_URL = "http://localhost:3000/api/posts";
+const API_BASE_URL = "http://localhost:3000/";
+
+window.onload = () => {
+    getPosts();
+}
+
+const getPosts = () => {
+}
+
+const buildPosts = (blogPosts) => {
+
+}
