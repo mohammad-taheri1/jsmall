03d128902c677f98bbf44fb9eaca78ee59b9ad5f
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-26 18:12:05 +0330

    make posts fully dynamic

--- js/index.js
+++ js/index.js
@@ -8,8 +8,32 @@ window.onload = () => {
 }
 
 const getPosts = () => {
+    fetch(API_URL, {
+        method: 'GET'
+    })
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        buildPosts(data);
+    })
 }
 
 const buildPosts = (blogPosts) => {
-
+    let blogPostsContent = "";
+    for(blogPost of blogPosts) {
+        const postDate = new Date(parseInt(blogPost.added_date)).toDateString();
+        const postImage = `${API_BASE_URL}${blogPost.post_image}`;
+        blogPostsContent += `
+            <div class="post">
+                <div class="post-image" style="background-image: url(${postImage})"></div>
+                <div class="post-content">
+                    <div class="post-date">${postDate}</div>
+                    <div class="post-title"><h4>${blogPost.title}</h4></div>
+                    <div class="post-text">${blogPost.content}</div>
+                </div>
+            </div>
+        `
+    }
+    document.querySelector(".blog-posts").innerHTML = blogPostsContent;
 }
