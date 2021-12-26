b7e6ae0c75b53d5c946084b2d1516bd5d13ac6c9
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-27 00:29:26 +0330

    handle new-post

--- js/new-post.js
+++ js/new-post.js
@@ -7,4 +7,23 @@ const API_URL = "http://localhost:3333/api/posts";
 const submitNewPost = () => {
     // HINT: Use FormData to store data to send over
     // HINT: Redirect the user to home page after successful submission
+    const input = document.getElementById("form-post-image");
+    const title = document.getElementById("form-post-title").value;
+    const content = document.getElementById("form-post-content").value;
+
+    let data = new FormData();
+    data.append("post-image", input.files[0]);
+    data.append("title", title);
+    data.append("content", content);
+
+    fetch(API_URL, {
+        method: "POST",
+        body: data
+    })
+    .then(() => {
+        setTimeout(() => {
+            window.location.href = "index.html";
+        }, 1000);
+    })
+
 }
